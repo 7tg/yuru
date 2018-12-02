6c400adee52176092641c2086de5edf47ccc22f0
tayyipgoren <tayyipgoren@gmail.com>
2018-12-02 15:27:26 +0300

    Changes

--- app/src/main/java/com/tayyipgoren/yuru/MainActivity.java
+++ app/src/main/java/com/tayyipgoren/yuru/MainActivity.java
@@ -2,12 +2,24 @@ package com.tayyipgoren.yuru;
 
 import android.graphics.Color;
 import android.support.annotation.ColorRes;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
 import devlight.io.library.ntb.NavigationTabBar;
 
@@ -17,6 +29,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.initUI();
+    }
+
+    private boolean initUI()
+    {
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
+        viewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 5;
+            }
+
+            @Override
+            public boolean isViewFromObject(final View view, final Object object) {
+                return view.equals(object);
+            }
+
+            @Override
+            public void destroyItem(final View container, final int position, final Object object) {
+                ((ViewPager) container).removeView((View) object);
+            }
+
+            @Override
+            public Object instantiateItem(final ViewGroup container, final int position) {
+                final View view = LayoutInflater.from(
+                        getBaseContext()).inflate(R.layout.item_vp_list, null, false);
+
+                final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rv);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(
+                                getBaseContext(), LinearLayoutManager.VERTICAL, false
+                        )
+                );
+                recyclerView.setAdapter(new RecyclerView.Adapter() {
+                    @Override
+                    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                        return null;
+                    }
+
+                    @Override
+                    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+                    }
+
+                    @Override
+                    public int getItemCount() {
+                        return 0;
+                    }
+                });
+
+                container.addView(view);
+                return view;
+            }
+        });
 
         final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
         String[] colors = getResources().getStringArray(R.array.colors);
@@ -62,24 +129,118 @@ public class MainActivity extends AppCompatActivity {
                         .build()
         );
         navigationTabBar.setModels(models);
-        // ViewPager viewPager = findViewById(R.id.vp_horizontal_ntb);
-        // navigationTabBar.setViewPager(viewPager, 2);
+        navigationTabBar.setViewPager(viewPager, 2);
+        navigationTabBar.setBehaviorEnabled(true);
 
-        navigationTabBar.setTitleMode(NavigationTabBar.TitleMode.ACTIVE);
-        navigationTabBar.setBadgeGravity(NavigationTabBar.BadgeGravity.BOTTOM);
-        navigationTabBar.setBadgePosition(NavigationTabBar.BadgePosition.CENTER);
+        navigationTabBar.setOnTabBarSelectedIndexListener(new NavigationTabBar.OnTabBarSelectedIndexListener() {
+            @Override
+            public void onStartTabSelected(final NavigationTabBar.Model model, final int index) {
+            }
+
+            @Override
+            public void onEndTabSelected(final NavigationTabBar.Model model, final int index) {
+                model.hideBadge();
+            }
+        });
+        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(final int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(final int state) {
+
+            }
+        });
+
+        final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.parent);
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                for (int i = 0; i < navigationTabBar.getModels().size(); i++) {
+                    final NavigationTabBar.Model model = navigationTabBar.getModels().get(i);
+                    navigationTabBar.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            final String title = String.valueOf(new Random().nextInt(15));
+                            if (!model.isBadgeShowed()) {
+                                model.setBadgeTitle(title);
+                                model.showBadge();
+                            } else model.updateBadgeTitle(title);
+                        }
+                    }, i * 100);
+                }
+
+                coordinatorLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        final Snackbar snackbar = Snackbar.make(navigationTabBar, "Coordinator NTB", Snackbar.LENGTH_SHORT);
+                        snackbar.getView().setBackgroundColor(Color.parseColor("#9b92b3"));
+                        ((TextView) snackbar.getView().findViewById(R.id.snackbar_text))
+                                .setTextColor(Color.parseColor("#423752"));
+                        snackbar.show();
+                    }
+                }, 1000);
+            }
+        });
+
+        final CollapsingToolbarLayout collapsingToolbarLayout =
+                (CollapsingToolbarLayout) findViewById(R.id.toolbar);
+        collapsingToolbarLayout.setExpandedTitleColor(Color.parseColor("#009F90AF"));
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.parseColor("#9f90af"));
+
+//        navigationTabBar.setTitleMode(NavigationTabBar.TitleMode.ACTIVE);
+//        navigationTabBar.setBadgeGravity(NavigationTabBar.BadgeGravity.BOTTOM);
+//        navigationTabBar.setBadgePosition(NavigationTabBar.BadgePosition.CENTER);
         navigationTabBar.setTypeface("fonts/custom_font.ttf");
         navigationTabBar.setIsBadged(true);
-        navigationTabBar.setIsTitled(true);
-        navigationTabBar.setIsTinted(true);
-        navigationTabBar.setIsBadgeUseTypeface(true);
-        navigationTabBar.setBadgeBgColor(Color.RED);
-        navigationTabBar.setBadgeTitleColor(Color.WHITE);
-        navigationTabBar.setIsSwiped(true);
-        navigationTabBar.setBgColor(Color.BLACK);
-        navigationTabBar.setBadgeSize(10);
-        navigationTabBar.setTitleSize(10);
-        navigationTabBar.setIconSizeFraction((float)(0.5));
+//        navigationTabBar.setIsTitled(true);
+//        navigationTabBar.setIsTinted(true);
+//        navigationTabBar.setIsBadgeUseTypeface(true);
+//        navigationTabBar.setBadgeBgColor(Color.RED);
+//        navigationTabBar.setBadgeTitleColor(Color.BLUE);
+//        navigationTabBar.setIsSwiped(true);
+//        navigationTabBar.setBgColor(Color.BLACK);
+//        navigationTabBar.setBadgeSize(10);
+//        navigationTabBar.setTitleSize(10);
+          navigationTabBar.setIsTinted(true);
+//        navigationTabBar.setIconSizeFraction((float)(0.2));
 
+        return true;
+    }
+
+    public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+            final View view = LayoutInflater.from(getBaseContext()).inflate(R.layout.item_list, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            holder.txt.setText(String.format("Navigation Item #%d", position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 20;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView txt;
+
+            public ViewHolder(final View itemView) {
+                super(itemView);
+                txt = (TextView) itemView.findViewById(R.id.txt_vp_item_list);
+            }
+        }
     }
 }
